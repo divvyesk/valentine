a50711814da54b9744a3733c95032774083a3fb1
divvyesk <divvyesk2428@gmail.com>
2026-02-07 20:53:24 +0530

    final ff

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Will-you-be-my-Valentine-">
+      <BrowserRouter basename="/valentine">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/Will-you-be-my-Valentine-/",
+  base: "/valentine/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
